8de7777aff7664b948d5d34b9b331470bf36117e
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2020-08-10 18:46:05 +0200

    style: added primereact css files in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import './index.css';
+import 'animate.css/animate.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HeroesApp } from './HeroesApp';
